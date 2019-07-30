aaed2795009f7bf89f23f91c2cc97bcf6b9edbb8
NiyatiShirthadi <44228758+NiyatiShirthadi@users.noreply.github.com>
2019-07-30 20:18:52 +0530

    Spill detections + Visual additions

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Waterdrop;
+
+    private int Number = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (Number < 5)
+        {
+            Instantiate(Waterdrop, transform.position, Quaternion.identity);
+            Number++;
+            //Debug.Log(Number);
+            //Debug.Log(transform.position);
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         
+        
+    }
+}
---
+++ Assets/Scripts/SpillCount.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpillCount : MonoBehaviour
+{
+    private static int SpillCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Curry")
+        {
+            SpillCounter++;
+            Debug.Log(SpillCounter);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
